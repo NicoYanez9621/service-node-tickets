a7b45b68547ae3fcd2f36a2afe1a83358d59770f
Airton Nicolas Yañez <airtonnicolasy1996@gmail.com>
2023-10-31 03:48:08 -0300

    agregue comentario para saber como probar local y que codigo descomentar para deployar

--- src/database/database.js
+++ src/database/database.js
@@ -3,11 +3,20 @@ import Sequelize from "sequelize";
 
 config();
 const DATABASE_URL = process.env.DATABASE_URL;
+const DATABASE_NAME = process.env.DATABASE_NAME;
+const DATABASE_USER = process.env.DATABASE_USER;
+const DATABASE_PASS = process.env.DATABASE_PASS;
 
 // usar de manera Local
-// export const sequelize = new Sequelize({
-//   host: `${DATABASE_URL}`,
-//   dialect: "postgres",
-// });
+// export const sequelize = new Sequelize(
+//   DATABASE_NAME,
+//   DATABASE_USER,
+//   DATABASE_PASS,
+//   {
+//     host: `${DATABASE_URL}`,
+//     dialect: "postgres",
+//   }
+// );
 
+//usar cuando deploye
 export const sequelize = new Sequelize(`${DATABASE_URL}`);
