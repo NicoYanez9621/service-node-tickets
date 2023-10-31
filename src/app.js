01e1a91ede208dffddef0eef9743e97963321f1d
Airton Nicolas Yañez <airtonnicolasy1996@gmail.com>
2023-10-31 11:22:27 -0300

    agregue las nuevas rutas del modelo al app

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import usersRoutes from "./routes/users.routes.js";
+import ticketsRoutes from "./routes/tickets.routes.js";
 
 const app = express();
 
@@ -7,5 +8,6 @@ const app = express();
 app.use(express.json());
 
 app.use("/api", usersRoutes);
+app.use("/api", ticketsRoutes);
 
 export default app;
