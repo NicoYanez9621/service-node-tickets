e9200ebdbb3e70343de5f461e1acbb4e192b0554
Airton Nicolas Yañez <airtonnicolasy1996@gmail.com>
2023-10-31 03:39:51 -0300

    cambie el llamado para realizar la conexion

--- src/database/database.js
+++ src/database/database.js
@@ -4,7 +4,10 @@ import Sequelize from "sequelize";
 config();
 const DATABASE_URL = process.env.DATABASE_URL;
 
-export const sequelize = new Sequelize({
-  host: `${DATABASE_URL}`,
-  dialect: "postgres",
-});
+// usar de manera Local
+// export const sequelize = new Sequelize({
+//   host: `${DATABASE_URL}`,
+//   dialect: "postgres",
+// });
+
+export const sequelize = new Sequelize(`${DATABASE_URL}`);
