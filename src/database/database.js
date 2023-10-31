c45e48044e1e53289b86c1f188b47e39a64e3961
Airton Nicolas Yañez <airtonnicolasy1996@gmail.com>
2023-10-31 03:01:53 -0300

    elimine archivos que habia creado para deployar en fly.io pero que no pude hacer funcionar

--- src/database/database.js
+++ src/database/database.js
@@ -7,11 +7,11 @@ const DATABASE_NAME = process.env.DATABASE_NAME;
 const DATABASE_USER = process.env.DATABASE_USER;
 const DATABASE_PASS = process.env.DATABASE_PASS;
 export const sequelize = new Sequelize(
-  DATABASE_NAME,
-  DATABASE_USER,
-  DATABASE_PASS,
+  `${DATABASE_NAME}`,
+  `${DATABASE_USER}`,
+  `${DATABASE_PASS}`,
   {
-    host: DATABASE_URL,
+    host: `${DATABASE_URL}`,
     dialect: "postgres",
   }
 );
