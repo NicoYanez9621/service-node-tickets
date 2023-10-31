9125f931cd75c341701d5318219b40f0f4ecc49f
Airton Nicolas Yañez <airtonnicolasy1996@gmail.com>
2023-10-31 03:23:01 -0300

    cambio para deployar el back

--- src/database/database.js
+++ src/database/database.js
@@ -2,16 +2,8 @@ import { config } from "dotenv";
 import Sequelize from "sequelize";
 
 config();
-const DATABASE_URL = process.env.DATABASE_URL;
-const DATABASE_NAME = process.env.DATABASE_NAME;
-const DATABASE_USER = process.env.DATABASE_USER;
-const DATABASE_PASS = process.env.DATABASE_PASS;
-export const sequelize = new Sequelize(
-  `${DATABASE_NAME}`,
-  `${DATABASE_USER}`,
-  `${DATABASE_PASS}`,
-  {
-    host: `${DATABASE_URL}`,
-    dialect: "postgres",
-  }
-);
+
+export const sequelize = new Sequelize({
+  host: `${DATABASE_URL}`,
+  dialect: "postgres",
+});
