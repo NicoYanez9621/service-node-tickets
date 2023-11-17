0db89e31df6ccf0f43df0850a575365b1de9d8bd
Airton Nicolas Yañez <airtonnicolasy1996@gmail.com>
2023-11-17 19:18:55 -0300

    fix import de cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-const cors = require("cors");
+import cors from "cors";
 import remitosRoutes from "./routes/remitos.routes.js";
 import productosRoutes from "./routes/productos.routes.js";
 import proveedoresRoutes from "./routes/proveedores.routes.js";
